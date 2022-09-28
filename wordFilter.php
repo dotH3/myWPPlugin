f466e7235fd056b4144e25349a80ff58c9586820
h3 <joaquinbastias321@gmail.com>
2022-09-28 03:31:09 -0300

    28/09/22

--- wordFilter.php
+++ wordFilter.php
@@ -16,13 +16,48 @@ class OurWordFilterPlugin {
     }
     
     function ourMenu() {
-        add_menu_page('Words To Filter', 'Word Filter', 'manage_options', 'ourwordfilter', array($this, 'wordFilterPage'), 'dashicons-smiley', 100);
+        $mainPageHook = add_menu_page('Words To Filter', 'Word Filter', 'manage_options', 'ourwordfilter', array($this, 'wordFilterPage'), 'dashicons-smiley', 100);
         add_submenu_page( 'ourwordfilter', 'Word To Filter', 'Words List', 'manage_options', 'ourwordfilter', array($this, 'wordFilterPage') );
         add_submenu_page( 'ourwordfilter', 'Word Filter Optiones', 'Options', 'manage_options', 'word-filter-options', array($this, 'optionsSubPage') );
+        add_action("load-{$mainPageHook}", array($this, 'mainPageAssets'));
     }
 
+    function mainPageAssets() {
+        wp_enqueue_style( 'filterAdminCss', plugin_dir_url( __FILE__ ) . 'style.css');
+    }
+
+    function handleForm() {
+    if (wp_verify_nonce($_POST['ourNonce'], 'saveFilterWords') AND current_user_can('manage_options')) {
+      update_option('plugin_words_to_filter', sanitize_text_field($_POST['plugin_words_to_filter'])); ?>
+      <div class="updated">
+        <p>Your filtered words were saved.</p>
+      </div>
+    <?php } else { ?>
+      <div class="error">
+        <p>Sorry, you do not have permission to perform that action.</p>
+      </div>
+    <?php } 
+  }
+
     function wordFilterPage() { ?>
-        Hola !  
+        <div class="wrap">
+            <h1>Word Filter</h1>
+            <?php if($_POST['justsubmitted'] == 'true') $this->handleForm() ?>
+            <form method="POST">
+                <input type="hidden" name="justsubmitted" value="true">
+                <?php 
+                // wp_nonce_field( action, name, referer, echo )
+                wp_nonce_field('saveFilterWords', 'ourNonce') 
+                ?>
+                <label for="plugin_word_to_filter"><p>Enter <strong>"," (Coma)</strong> to separate the words</p></label>
+                <div class="word-filter__flex-container">
+                    <textarea name="plugin_words_to_filter" id="plugin_words_to_filter">
+                        <?php echo esc_textarea(get_option('plugin_words_to_filter')) ?>
+                    </textarea>
+                </div>
+                <input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes">
+            </form>
+        </div>
     <?php }
 
     function optionsSubPage() { ?>
