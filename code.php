d8512da25ea025a6b8bd729d5d244dcb97b95e9e
h3 <joaquinbastias321@gmail.com>
2022-09-29 12:14:17 -0300

    29/09/22

--- code.php
+++ code.php
@@ -1,6 +1,6 @@
 <?php
     /**
-     * Plugin Name: myCarousel
+     * Plugin Name: myWPPlugin - Carousel
      */
 
     if( ! defined( 'ABSPATH' ) ) exit;
@@ -29,8 +29,10 @@
         function adminMenu() {
             $mainPageHook = add_menu_page( 'Custom Carousel Page', 'My Carousel', 'manage_options', 'custom-carousel', array($this, 'adminPageHTML'), 'dashicons-smiley', 100 );
             // Load CSS
+            add_submenu_page( 'custom-carousel', 'Upload Image Page', 'Upload', 'manage_options', 'upload-img', array($this, 'adminSubPageHTML'));
             add_action("load-{$mainPageHook}", array($this, 'mainPageAssets'));
         }
+
         function adminPageHTML() { ?>
             <div class="wrap">
                 <h1>My Custom Carousel</h1>
@@ -43,6 +45,58 @@
                 </form>
             </div>
         <?php }
+
+        function adminSubPageHTML() { ?>
+            <div class="wrap">
+		        <h1>Ejemplo de subida de archivo</h1>
+		        <br>
+                <form enctype="multipart/form-data" method="post">
+                    Selecciona algún archivo: <input name="upload-file" type="file" /> <hr>
+                    <input type="submit" value="Enviar archivo" />
+                </form>
+                <script>
+                    ()=>{
+                        console.log("<?php 
+                            global $wp_filesystem;
+                            WP_Filesystem();
+                            // $content_directory = $wp_filesystem->wp_content_dir() . 'uploads/archivos-subidos/';
+                            $content_directory = $wp_filesystem->wp_content_dir();
+                            echo $content_directory;
+                        ?>");
+                    }
+                </script>
+	        </div>
+
+            <?php
+            if(isset($_FILES['upload-file'])) {
+                global $wp_filesystem;
+                WP_Filesystem();
+
+                $name_file = $_FILES['upload-file']['name'];
+                $tmp_name = $_FILES['upload-file']['tmp_name'];
+                $allow_extensions = ['png', 'jpg'];
+
+                // File type validation
+		        $path_parts = pathinfo($name_file);
+		        $ext = $path_parts['extension'];
+
+                if ( ! in_array($ext, $allow_extensions) ) {
+                    echo "Error - File type not allowed";
+                    return;
+                }
+
+                // $content_directory = $wp_filesystem->wp_content_dir(). 'uploads/';
+                $content_directory = $wp_filesystem->wp_content_dir() . 'uploads/archivos-subidos/';
+                $wp_filesystem->mkdir( $content_directory );
+                
+                if( move_uploaded_file( $tmp_name, $content_directory . $name_file ) ) {
+                    echo "File was successfully uploaded";
+                } else {
+                    echo "The file was not uploaded";
+                }
+
+            }
+        }
 
 
     }
